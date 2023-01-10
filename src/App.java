9cc46372e214cd8e1c2f5a0dffe89b1d9f456b80
Lucas Palharini <lucas230991@hotmail.com>
2023-01-10 13:48:47 -0300

    :pencil: docs: Acrescentando alguns comentarios

--- src/App.java
+++ src/App.java
@@ -7,9 +7,9 @@ public class App {
 
         boolean teveErro = false;
 
+        //Pede para o usuário digitar a entrada e fiscaliza se é válida
         try(Scanner entrada = new Scanner(System.in)){
 
-            //Aqui você escolhe qual número será multiplicado
             final int numero = entrada.nextInt();
 
             boolean temCabecalho = false;
@@ -26,6 +26,7 @@ public class App {
                     comprimento = linha.length();
                 }
 
+                //Cria o topo da tabuada, caso não tenha sido feita
                 if(!temCabecalho) {
                     System.out.println("\n╔" + "═".repeat(comprimento - 2) + "╗");
                     System.out.println("║" + " ".repeat(comprimento - 2)+ "║");
@@ -36,31 +37,31 @@ public class App {
 
                 System.out.println(linha + " ".repeat(comprimento - linha.length() - 1) + "║");
 
-
-
             }
+
+            //Cria o final da tabuada.
             System.out.println("║" + " ".repeat(comprimento - 2)+ "║");
             System.out.println("╚" + "═".repeat(comprimento - 2) + "╝");
+        
         }
         catch (Exception e) {
-            // TODO: handle exception
+            // Trata os erros, identificados pelo "hashCode"
             int error = e.hashCode();
 
-            if(error == 1828972342) {
+            if(error == 1828972342) { //Caso o usuário ultrapasse o tamanho do 'int'
                 System.out.println("\n É uma tabuada, Raio de sol,\n você não tem nem capacidade\n de ler esse número!");
-            } else if(error == 81628611) {
+            } else if(error == 81628611) { //Caso o usuário não coloque um número
                 System.out.println("\n Você precisa digitar um número!");
             }
             
             teveErro = true;
         }
         finally {
-            if(teveErro){
+            if(teveErro){ // :p
                 System.out.println("\n¯\\_('-')_/¯  Você é um bocózinho!\n\n");
             } else {
                 System.out.println("\n(~ 'v')~ Tá pronto o sorvetinho!\n\n");
             }
         }
-
     }
 }
